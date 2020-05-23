9a9d8f1e987c46f262440753698a70d9f676ac5f
AitorCour <aitorcour@gmail.com>
2020-05-23 14:43:11 +0200

    conseguido pasar un modelo correctamentge de blender

---
+++ electro_ninja/Assets/Scripts/Slime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    private float upForce = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddExplosionForce(5, transform.position, 30, upForce, ForceMode.Impulse);
+        }
+        
+    }
+}
