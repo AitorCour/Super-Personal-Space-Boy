1f93c296def8a1b74d72b3126e2e9d17b47701d2
AitorCour <43616556+AitorCour@users.noreply.github.com>
2021-06-16 21:28:45 +0200

    Level 6, Mando xbox

--- electro_ninja/Assets/Scripts/InputManager.cs
+++ electro_ninja/Assets/Scripts/InputManager.cs
@@ -7,11 +7,13 @@ public class InputManager : MonoBehaviour
     // Start is called before the first frame update
     private Vector3 axis;
     private Vector2 axis2;
+    private Vector2 axis3;
     private PlayerBehaviour player;
     private UI_Manager ui;
     private GameManager gameManager;
     private DynamicJoystick joystick;
     public bool mobileCtrl;
+    public bool xboxController;
 
     public void Initialize()
     {
@@ -26,11 +28,17 @@ public class InputManager : MonoBehaviour
     {
         Vector3 axis = Vector3.zero;
         Vector2 axis2 = Vector2.zero;
-        if (!mobileCtrl)
+        Vector2 axis3 = Vector2.zero;
+        if (!mobileCtrl && !xboxController)
         {
             axis2.x = Input.GetAxis("Horizontal");
             //axis.z = Input.GetAxis("Vertical");
             axis2.y = Input.GetAxis("Vertical");
+        }
+        if(xboxController)
+        {
+            axis3.x = Input.GetAxisRaw("J_Horizontal");
+            axis3.y = Input.GetAxisRaw("J_Vertical");
         }
         else
         {
@@ -38,14 +46,15 @@ public class InputManager : MonoBehaviour
             axis.z = joystick.Direction.y;
         }
         player.Move(axis2);
+        player.Move(axis3);
 
-        if(Input.GetButtonDown("Jump") && !ui.cooling)
+        if (Input.GetButtonDown("Jump") && !ui.cooling)
         {
             player.Attack();
             ui.StartCooldown();
             //cooldown
         }
-        if(Input.GetKeyDown(KeyCode.R))
+        if(Input.GetKeyDown(KeyCode.R) || Input.GetButtonDown("J_Reboot"))
         {
             gameManager.Restart();
         }
